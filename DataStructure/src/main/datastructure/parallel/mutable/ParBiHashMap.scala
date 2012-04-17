4e31de02e3aac091c63ec35184d0e62bfbcf001c
TheBizzle <jason.bertsche@gmail.com>
2012-04-17 00:28:12 -0500

    Big steps forward on a 'proper' mutable BiHashMap

---
+++ DataStructure/src/main/datastructure/parallel/mutable/ParBiHashMap.scala
@@ -0,0 +1,65 @@
+package datastructure.parallel
+package mutable
+
+/**
+* Created by IntelliJ IDEA.
+* User: Jason
+* Date: 4/16/12
+* Time: 10:12 PM
+*/
+
+import collection.{GenTraversableOnce, GenIterable}
+import collection.parallel.mutable.{ParHashTable, ParMapLike, ParMap}
+import collection.parallel.Combiner
+import collection.generic._
+import collection.mutable.DefaultEntry
+
+
+//@ Does not even come close to working right now.  And I do not really care (for the time being).
+@SerialVersionUID(1L)
+class ParBiHashMap[A, B] private[datastructure](contents: (A, B)*)
+extends ParMap[A, B]
+   with GenericParMapTemplate[A, B, ParBiHashMap]
+   with ParMapLike[A, B, ParBiHashMap[A, B], datastructure.mutable.BiHashMap[A, B]]
+   with ParHashTable[A, DefaultEntry[A, B]] //@ Doubtful
+   with Serializable
+{
+  override def mapCompanion: GenericParMapCompanion[ParBiHashMap] = ParBiHashMap
+  override def empty: ParBiHashMap[A, B] = new ParBiHashMap[A, B]
+  protected[this] override def newCombiner = throw new UnsupportedOperationException("")
+  def size = throw new UnsupportedOperationException("")
+  override def scan[Y >: (A, B), That](z: Y)(op: (Y, Y) => Y)(implicit cbf: CanBuildFrom[ParBiHashMap[A, B], Y, That]) = throw new UnsupportedOperationException("")
+  override def scanLeft[Y, That](z: Y)(op: (Y, (A, B)) => Y)(implicit bf: CanBuildFrom[ParBiHashMap[A, B], Y, That]) = throw new UnsupportedOperationException("")
+  override def scanRight[Y, That](z: Y)(op: ((A, B), Y) => Y)(implicit bf: CanBuildFrom[ParBiHashMap[A, B], Y, That]) = throw new UnsupportedOperationException("")
+  override def map[Y, That](f: ((A, B)) => Y)(implicit bf: CanBuildFrom[ParBiHashMap[A, B], Y, That]) = throw new UnsupportedOperationException("")
+  def get(key: A) = throw new UnsupportedOperationException("")
+  override def collect[Y, That](pf: PartialFunction[(A, B), Y])(implicit bf: CanBuildFrom[ParBiHashMap[A, B], Y, That]) = throw new UnsupportedOperationException("")
+  override def flatMap[Y, That](f: ((A, B)) => GenTraversableOnce[Y])(implicit bf: CanBuildFrom[ParBiHashMap[A, B], Y, That]) = throw new UnsupportedOperationException("")
+  override def ++[Y >: (A, B), That](that: GenTraversableOnce[Y])(implicit bf: CanBuildFrom[ParBiHashMap[A, B], Y, That]) = throw new UnsupportedOperationException("")
+  override def zip[X1 >: (A, B), Y, That](that: GenIterable[Y])(implicit bf: CanBuildFrom[ParBiHashMap[A, B], (X1, Y), That]) = throw new UnsupportedOperationException("")
+  override def zipWithIndex[X1 >: (A, B), That](implicit bf: CanBuildFrom[ParBiHashMap[A, B], (X1, Int), That]) = throw new UnsupportedOperationException("")
+  override def zipAll[Y, X1 >: (A, B), That](that: GenIterable[Y], thisElem: X1, thatElem: Y)(implicit bf: CanBuildFrom[ParBiHashMap[A, B], (X1, Y), That]) = throw new UnsupportedOperationException("")
+  def seq = throw new UnsupportedOperationException("")
+  def put(key: A, value: B) = throw new UnsupportedOperationException("")
+  def +=(kv: (A, B)) = throw new UnsupportedOperationException("")
+  def -=(key: A) = throw new UnsupportedOperationException("")
+  def clear() { throw new UnsupportedOperationException("") }
+  override def splitter = throw new UnsupportedOperationException("")
+}
+
+object ParBiHashMap extends ParMapFactory[ParBiHashMap] {
+  var iters = 0
+  def empty[A, B]: ParBiHashMap[A, B] = new ParBiHashMap[A, B]
+  def newCombiner[A, B]: Combiner[(A, B), ParBiHashMap[A, B]] = throw new UnsupportedOperationException("")
+  implicit def canBuildFrom[A, B]: CanCombineFrom[Coll, (A, B), ParBiHashMap[A, B]] = throw new UnsupportedOperationException("")
+}
+
+private[parallel] object ParHashMapCombiner {
+  private[mutable] val discriminantbits = throw new UnsupportedOperationException("")
+  private[mutable] val numblocks = throw new UnsupportedOperationException("")
+  private[mutable] val discriminantmask = throw new UnsupportedOperationException("")
+  private[mutable] val nonmasklength = throw new UnsupportedOperationException("")
+
+  def apply[A, B] = throw new UnsupportedOperationException("") // was: with EnvironmentPassingCombiner[(K, V), ParHashMap[K, V]]
+}
+
