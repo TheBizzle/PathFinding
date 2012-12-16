f1eda2f74ddaeaf611a46573287e58a1007deac5
TheBizzle <jason.bertsche@gmail.com>
2012-12-15 21:39:59 -0600

    I don't need all of those lambda methods with the chosen implementation

--- DataStructure/src/main/datastructure/mutable/Bijection.scala
+++ DataStructure/src/main/datastructure/mutable/Bijection.scala
@@ -50,29 +50,7 @@ trait Bijection[A, B, M[X, Y] <: MMap[X, Y], Rpr[X, Y] <: Bijection[X, Y, M, Rpr
   override def iterator : Iterator[Tup] = abMap.iterator
 
   // Lambda-operation methods
-  override def /:[C]                      (z: C)(op: (C, Tup) => C)                          : C           =   abMap./:(z)(op)
-  override def /:\[A1 >: Tup]             (z: A1)(op: (A1, A1) => A1)                        : A1          =   abMap./:\(z)(op)
-  override def :\[C]                      (z: C)(op: (Tup, C) => C)                          : C           =   abMap.:\(z)(op)
-  override def aggregate[C]               (z: C)(seqop: (C, Tup) => C, combop: (C, C) => C)  : C           =   abMap.aggregate(z)(seqop, combop)
-  override def collectFirst[C]            (pf: PartialFunction[Tup, C])                      : Option[C]   =   abMap collectFirst pf
-  override def count                      (p: (Tup) => Boolean)                              : Int         =   abMap count p
-  override def exists                     (p: (Tup) => Boolean)                              : Boolean     =   abMap exists p
-  override def find                       (p: (Tup) => Boolean)                              : Option[Tup] =   abMap find p
-  override def fold[A1 >: Tup]            (z: A1)(op: (A1, A1) => A1)                        : A1          =   abMap.fold(z)(op)
-  override def foldLeft[C]                (z: C)(op: (C, Tup) => C)                          : C           =   abMap.foldLeft(z)(op)
-  override def foldRight[C]               (z: C)(op: (Tup, C) => C)                          : C           =   abMap.foldRight(z)(op)
-  override def forall                     (p: (Tup) => Boolean)                              : Boolean     =   abMap forall p
   override def foreach[C]                 (f: (Tup) => C)                                                    { abMap.foreach(f) }
-  override def minBy[C]                   (f: (Tup) => C)(implicit cmp: Ordering[C])         : Tup         =   abMap.minBy(f)
-  override def maxBy[C]                   (f: (Tup) => C)(implicit cmp: Ordering[C])         : Tup         =   abMap.maxBy(f)
-  override def reduce[C >: Tup]           (op: (C, C) => C)                                  : C           =   abMap reduce op
-  override def reduceLeft[C >: Tup]       (op: (C, Tup) => C)                                : C           =   abMap reduceLeft op
-  override def reduceLeftOption[C >: Tup] (op: (C, Tup) => C)                                : Option[C]   =   abMap reduceLeftOption op
-  override def reduceOption[C >: Tup]     (op: (C, C) => C)                                  : Option[C]   =   abMap reduceOption op
-  override def reduceRight[C >: Tup]      (op: (Tup, C) => C)                                : C           =   abMap reduceRight op
-  override def reduceRightOption[C >: Tup](op: (Tup, C) => C)                                : Option[C]   =   abMap reduceRightOption op
-  override def retain                     (p: (A, B) => Boolean)                             : this.type   = { this.seq foreach { case (k, v) => if (!p(k, v)) this -= k }; this }
-  override def transform                  (f: (A, B) => B)                                   : this.type   = { this.iterator foreach { case (k, v) => update(k, f(k, v)) }; this }
   override def withDefault                (d: A => B)                                        : MMap[A, B]  =   abMap withDefault d   //@ I'd love to do this with a better return type...
 
   def sameElements[U : ((A, B) || (B, A))#T, C >: U](that: collection.GenIterable[C]) : Boolean = (abMap sameElements that) || (baMap sameElements that)
