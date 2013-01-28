4f73230f33383c89a98a2a1974e38e1b3c682277
TheBizzle <jason.bertsche@gmail.com>
2013-01-27 23:43:12 -0600

    Improving code quality of 'TestFunction'

--- Tester/src/main/tester/cluster/TestFunction.scala
+++ Tester/src/main/tester/cluster/TestFunction.scala
@@ -14,22 +14,24 @@ import
  */
 
 abstract class TestFunction[T <: Testable, Subject <: TestSubject, Status <: ExecutionStatus, AnalysisFlags <: TestAnalysisFlagBundle, AnalysisResult <: TestAnalysisResultBundle]
-                           (subject: Subject, analysisFunction: (Status, AnalysisFlags) => AnalysisResult, testNumber: Int, shouldPass: Boolean) extends ((T, TestFuncFlagBundle) => Boolean) {
+                           (subject: Subject, analysisFunction: (Status, AnalysisFlags) => AnalysisResult, testNumber: Int, shouldPass: Boolean)
+    extends ((T, TestFuncFlagBundle) => Boolean) {
 
-  val testNum = testNumber
+  val testNum       = testNumber
   val shouldSucceed = shouldPass
-  protected val testSubject = subject
+
+  protected val testSubject  = subject
   protected val analysisFunc = analysisFunction
 
   override def apply(testable: T, testFlags: TestFuncFlagBundle) : Boolean
+
   protected def extractAnalysisFlags(testFlags: TestFuncFlagBundle) : AnalysisFlags
 
 }
 
 trait TestFuncConstructionBundle
 
-class TestFuncFlagBundle(inToggles: Set[TestToggleFlag], extras: Set[TestToggleFlag] = Set())
-  extends ToggleFlagManager(inToggles, extras + Talkative)
+class TestFuncFlagBundle(inToggles: Set[TestToggleFlag], extras: Set[TestToggleFlag] = Set()) extends ToggleFlagManager(inToggles, extras + Talkative)
 
 trait TestFunctionFactory[T <: TestFunction[_, U, _, _, _], U <: TestSubject, V <: TestFuncConstructionBundle] {
 
@@ -37,12 +39,12 @@ trait TestFunctionFactory[T <: TestFunction[_, U, _, _, _], U <: TestSubject, V 
 
   // Essentially, uses reflection to find to find all T-type fields of PathingTestCluster
   def generateTests : Seq[T] = {
-    val genFunc = generateTestFunction(_: (Field, String), regex = TestFunctionRegex)  // Partial application
+    val genFunc     = generateTestFunction(_: (Field, String), regex = TestFunctionRegex)  // Partial application
     val fieldTuples = this.getClass.getDeclaredFields map (x => (x, x.getName))
-    fieldTuples map genFunc collect { case Some(x) => x } toSeq
+    (fieldTuples map genFunc).toSeq.flatten
   }
 
-  protected def generateTestFunction(fieldData: (Field, String), regex: String) : Option[T]
+  protected def generateTestFunction(fieldData: (Field, String), regex: String)        : Option[T]
   protected def construct(subject: U, testNumber: Int, shouldPass: Boolean, bundle: V) : T
 
 }
