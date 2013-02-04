f16ee93333f2dfb2cdd9fa537ed00de9388a299e
TheBizzle <jason.bertsche@gmail.com>
2013-02-03 21:40:54 -0600

    Checking in frustrated commentary
    PS I hate SBT

--- project/Build.scala
+++ project/Build.scala
@@ -21,4 +21,28 @@ object PathFindingBuild extends Build {
   lazy val omniTest   = TaskKey[Unit]("omni",        "Runs all tests")
   lazy val omniQuiet  = TaskKey[Unit]("omni-quiet",  "Runs all tests (quietly)")
 
+
+/*
+
+  val x = omniTest <<= state map {
+    (s: State) =>
+      mainClass in (Compile, run) := Option("astar.OmniTest")
+      Project.runTask(Keys.run in Compile, s)
+  }
+
+  val e = omniTest <<= run { x =>
+    (x, scalaVersion) map { (x, y) =>
+      mainClass in Compile := Option("astar.OmniTest")
+      println(x)
+      println(y)
+    }
+  }
+
+(omniTest in Test) <<= runTask(Test, "astar.OmniTest") dependsOn (compile, copyResources)
+mainClass in (Test, omniTest) := Option("astar.OmniTest")
+
+// It works to set `mainClass in Test := Option("astar.OmniTest")` and then call `test:run` in the SBT console.  Surely, there's some way to do better.  I don't know it, though
+
+ */
+
 }
