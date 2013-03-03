798b295b1716e9618053d09630bb8c1b3a69258f
TheBizzle <jason.bertsche@gmail.com>
2013-03-02 19:49:21 -0600

    Adding a package object for type alias

---
+++ AStar/src/main/org/bizzle/astar/base/package.scala
@@ -0,0 +1,13 @@
+package org.bizzle.astar
+
+import org.bizzle.pathfinding.coordinate.{ Coordinate2D, PriorityCoordinate }
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jason
+ * Date: 3/3/13
+ * Time: 9:47 AM
+ */
+package object base {
+  protected type PFCoord = Coordinate2D with PriorityCoordinate
+}
